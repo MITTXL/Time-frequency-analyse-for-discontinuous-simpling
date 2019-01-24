ec00058b398a163679b63b039af944e83fde84e7
Aska Hu <huhaozhou@live.com>
2019-01-24 11:40:25 -0500

    加入处理非均匀缺陷信号

--- FFT_LS.m
+++ FFT_LS.m
@@ -1,44 +1,63 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 最小二乘替代FFT
+% 最小二乘替代
 % 时间：20180106
 % 附属函数脚本：无
-% change log：Initial release
+% change log：加入处理非均匀缺陷信号
+% (Caution) 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 clc;clear
-fs = 800;
+fs = 512;
 % 均匀时间
-t_even = 0:1/fs:1-1/fs;
-t_un = sort(rand(1,fs));
-N = length(t_even);
-
-% theta = zeros(N+1,1);
-phi = ones(length(t_even),N-1);
-y_even = sin(2*pi*(30*t_even'.^2+40*t_even'));
-y_un = sin(2*pi*(30*t_un'.^2+40*t_un'));
-% y_un = 20*cos(2*pi*100*t_un') + 10*sin(2*pi*15*t_un');
-
-for n = 1:N/2-1
-    phi(:,2*n) = sin(2*pi*n*fs/N*t_even);
-    phi(:,2*n+1) = cos(2*pi*n*fs/N*t_even);
+t = 1/fs:1/fs:2;
+N = length(t);
+sig = sin(2*pi*(20*t'.^2 + 10*t'));
+%% 制造缺失区间
+jump_op = 319; % [可调] 
+jimp_ed = 320; % [可调] 
+t_ls = t([1:jump_op,jimp_ed:end]);
+sig_ls = sig([1:jump_op,jimp_ed:end]);
+N_ls = length(t_ls);
+continue_rate = 4/4; % [可调] 
+%% 无约束最小二乘数值解
+% 问题：直流分量出现在第一行会导致结果出错
+% 求矩阵逆时报错并产生不理想的结果，论文中基函数顺序建议调整
+freq_range = [fs/N:fs/N:fs/2];
+freq_vector = freq_range(1:floor(length(freq_range)*continue_rate));
+phi = ones(N_ls, length(freq_vector)*2+1);
+for n = 1:length(freq_vector)
+    phi(:,2*n-1) = sin(2*pi*freq_vector(n)*t_ls);
+    phi(:,2*n) = cos(2*pi*freq_vector(n)*t_ls);
 end
+theta = phi\sig_ls;
 
-% 核查theta公式是否错误
-% theta = (phi'*phi)\phi'*y_un;
-theta = (y_even\phi)';
-y_hat = phi*theta;
+%% 重构信号
+resample_rete = 100; % [可调] 
+t_full = min(t):1/fs/resample_rete:max(t);
+freq_range_re = [fs/N/resample_rete:fs/N/resample_rete:fs/2/resample_rete];
+phi = zeros(length(freq_vector)*2+1, length(t_full));
+phi(end,:)=1;
+for i = 1:length(freq_vector)
+    phi(2*i-1,:) = sin(2*pi*freq_vector(i)*t_full);
+    phi(2*i, :)  = cos(2*pi*freq_vector(i)*t_full);
+end
+sig_re = phi'*theta;
+figure,plot(t_full, sig_re/max(sig_re))
+hold on
+plot(t,sig,'g.')
+hold off
+title('LS拟合 未加约束')
 
-
-ft = abs(fftshift(fft(y_even)));
-L = zeros(N/2-1,1);
-L(1) = theta(1);
-for n = 1:N/2-1
-    L(n+1) = sqrt(theta(2*n-1)^2 + theta(2*n)^2);
+%%
+ft = abs(fftshift(fft(sig)));
+L = zeros(N/2+1,1);
+L(N/2+1) = theta(end);
+for n = 1:N/2
+    L(n) = sqrt(theta(2*n-1)^2 + theta(2*n)^2);
 end
 
 ft = ft/max(ft);
 L = L/max(L);
-figure,plot(0:1:length(L)-1, L),hold on
-plot(0:1:(length(ft)/2),ft(length(ft)/2:length(ft))),hold off
+figure,plot(L),hold on
+plot(ft(length(ft)/2+2:length(ft))),hold off
 
-err = abs(y_even - y_hat);
+
